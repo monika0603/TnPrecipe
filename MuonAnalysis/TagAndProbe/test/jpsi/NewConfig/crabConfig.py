f8abfca7a12d1b67758c4cf00aa24a857afdf408
monika0603 <monika.pu@gmail.com>
2015-07-03 17:48:59 +0200

    Crab config to process /JpsiToMuMu_OniaMuonFilter_TuneCUEP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/AODSIM

--- MuonAnalysis/TagAndProbe/test/jpsi/NewConfig/crabConfig.py
+++ MuonAnalysis/TagAndProbe/test/jpsi/NewConfig/crabConfig.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.section_('General')
-config.General.requestName = 'TagAndProbeFirstAttempt'
+config.General.requestName = 'TagAndProbe'
 config.General.workArea = 'TnP'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -13,10 +13,11 @@ config.JobType.psetName = 'tp_from_aod_simple_MC.py'
 config.JobType.outputFiles = ['tnpJpsi_MC.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/JpsiToMuMu_JPsiPt7_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM'
+config.Data.inputDataset = '/JpsiToMuMu_OniaMuonFilter_TuneCUEP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/AODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
+config.Data.ignoreLocality = True
 #config.Data.lumiMask = 'Cert_181530-183126_HI7TeV_PromptReco_Collisions11_JSON.txt'
 config.Data.publication = False
 
